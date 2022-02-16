501fac46d334b9d572b2875ce4f9007ebbfe7e86
hyebin829 <smilerain779@gmail.com>
2022-02-16 21:45:45 +0900

    Chore: homepostcontent 주석 제거

--- client/src/components/HomePostContent.js
+++ client/src/components/HomePostContent.js
@@ -15,61 +15,12 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CommentForm from './CommentForm';
 
 const HomePostContent = ({ post }) => {
-  const [anchorElUser, setAnchorElUser] = useState(null);
   const dispatch = useDispatch();
-
-  const handleOpenUserMenu = event => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <div>
       <PostImage images={post.Images} />
       {post.content}
-      {/* <CommentForm post={post} />
-      <List>
-        <ListItem alignItems="flex-start">
-      {post.Comment.map(()=>{
-        <>
-        <ListItemText
-            primary="작성자"
-            secondary={<React.Fragment>{'댓글 내용'}</React.Fragment>}
-          />
-          <Box>
-            <MoreVertIcon onClick={handleOpenUserMenu} />
-            <Menu
-              sx={{ mt: '25px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Stack>
-                  <Typography textAlign="left">수정</Typography>
-                  <Typography textAlign="left">삭제</Typography>
-                </Stack>
-              </MenuItem>
-            </Menu>
-          </Box>
-          </>
-      })}
-        </ListItem>
-        <Divider variant="middle" component="li" />
-      </List> */}
     </div>
   );
 };
