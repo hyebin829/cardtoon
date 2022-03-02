fa1080afc84450ed8f787751ad1de53a91754c85
hyebin829 <smilerain779@gmail.com>
2022-03-02 22:30:16 +0900

    Chore: theme letterspacing수정, profileform 수정

--- client/src/components/ProfileForm.js
+++ client/src/components/ProfileForm.js
@@ -3,37 +3,73 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import EditProfilePage from '../pages/editprofile';
 
-import { Avatar, Button } from '@mui/material';
+import { Avatar, Button, Box, Stack, Divider } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const List = styled.li`
+  list-style: none;
+`;
+
+const Nickname = styled.div`
+  margin-top: 10px;
+  font-size: 25px;
+  font-weight: 400;
+`;
 
 const ProfileForm = () => {
   const { user } = useSelector(state => state.user);
 
   return (
-    <>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        marginTop: '50px',
+      }}
+    >
       {user?.profileimagesrc === null ? (
-        <AccountCircleIcon sx={{ width: 120, height: 120 }} />
+        <AccountCircleIcon sx={{ width: 130, height: 130 }} />
       ) : (
         <Avatar
           src={`http://localhost:3065/${user?.profileimagesrc}`}
-          sx={{ width: 120, height: 120 }}
+          sx={{ width: 130, height: 130 }}
         />
       )}
-      <Button>
-        <Link to="/editprofile">수정</Link>
-      </Button>
-      <ul>
-        <div>
-          {user?.nickname === null ? '닉네임을 설정해주세요' : user?.nickname}
-        </div>
-        <li>게시글 {user?.Posts.length}개</li>
-        <li>팔로워 {user?.Followers.length} 명</li>
-        <li>팔로잉 {user?.Followings.length} 명</li>
-      </ul>
-    </>
+      <Nickname>
+        {user?.nickname === null ? '닉네임을 설정해주세요' : user?.nickname}
+      </Nickname>
+      <Link to="/editprofile" style={{ textDecoration: 'none' }}>
+        수정
+      </Link>
+      <Stack
+        direction="row"
+        spacing={2}
+        divider={<Divider orientation="vertical" flexItem />}
+        marginTop="20px"
+      >
+        <List>
+          <Stack>
+            <div>{user?.Posts.length}</div>
+            <div>게시글</div>
+          </Stack>
+        </List>
+        <List>
+          <Stack>
+            <div>{user?.Followers.length} </div>
+            <div>나를 즐겨찾는</div>
+          </Stack>
+        </List>
+        <List>
+          <Stack>
+            <div>{user?.Followings.length} </div>
+            <div>즐겨찾기</div>
+          </Stack>
+        </List>
+      </Stack>
+    </Box>
   );
 };
 
--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -1,31 +1,5 @@
 import { createTheme } from '@mui/material';
 
-// const deviceSize = {
-//   mobile: '375px',
-//   tabletM: '768px',
-//   tabletL: '1024px',
-//   desktop: '1460px',
-// };
-
-// const device = {
-//   mobile: `screen and (min-width: ${deviceSize.mobile})`,
-//   tabletM: `screen and (min-width: ${deviceSize.tabletM})`,
-//   tabletL: `screen and (min-width: ${deviceSize.tabletL})`,
-//   desktop: `screen and (min-width: ${deviceSize.desktop})`,
-// };
-
-// export const color = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#9163F2',
-//       // lightpurple: '#8480F2',
-//       // blue: '#30A8F2',
-//       // bluegreen: '#32BAD9',
-//       // green: '#48D9CA',
-//     },
-//   },
-// });
-
 const CustomMuiTheme = createTheme({
   // breakpoints: {
   //   keys: ['xs', 'sm', 'md', 'lg', 'xl'],
@@ -47,12 +21,12 @@ const CustomMuiTheme = createTheme({
   },
   typography: {
     fontSize: 15,
+    body1: {
+      letterSpacing: '-0.075em',
+    },
+    button: {
+      letterSpacing: '-0.075em',
+    },
   },
 });
 export default CustomMuiTheme;
-
-// const theme = {
-//   // device,
-// };
-
-// export default theme;
