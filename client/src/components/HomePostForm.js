be830a39a0a22d7e65ac5336cecafd467f85559a
hyebin829 <smilerain779@gmail.com>
2022-03-05 00:01:56 +0900

    Add: 게시글 작성 페이지 - 사진 업로드 수 제한 기능 추가 - 디자인 변경 - 게시글 글자수 제한 추가

--- client/src/components/HomePostForm.js
+++ client/src/components/HomePostForm.js
@@ -12,14 +12,32 @@ import MainMenu from '../components/MenuBar';
 import styled from 'styled-components';
 
 import TextField from '@mui/material/TextField';
-import { Button } from '@mui/material';
+import { Button, Grid, Stack } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
 
 const Input = styled.input`
   display: none;
 `;
 
+const ImagePreview = styled.img`
+  width: 170px;
+  height: 170px;
+  margin: 0 auto;
+  display: block;
+`;
+
+const ButtonWrap = styled.div`
+  text-align: right;
+  padding: 0 16px;
+`;
+
+const DeleteButtonWrap = styled.div`
+  text-align: right;
+`;
+
 const HomePostForm = () => {
   const [text, setText] = useState('');
+  const [imageLengthError, setImageLengthError] = useState(false);
   const dispatch = useDispatch();
   const { imagePaths } = useSelector(state => state.post);
   const navigate = useNavigate();
@@ -53,10 +71,15 @@ const HomePostForm = () => {
 
   const onChangeImages = useCallback(e => {
     console.log(e.target.files);
+
     const imageFormData = new FormData();
     [].forEach.call(e.target.files, f => {
       imageFormData.append('image', f);
     });
+    e.target.files.length > 10
+      ? setImageLengthError(true)
+      : setImageLengthError(false);
+    console.log(imageLengthError);
     return dispatch({
       type: UPLOAD_IMAGES_REQUEST,
       data: imageFormData,
@@ -68,39 +91,67 @@ const HomePostForm = () => {
       type: REMOVE_IMAGE,
       data: index,
     });
+    console.log(imagePaths);
+    imagePaths.length - 1 > 10
+      ? setImageLengthError(true)
+      : setImageLengthError(false);
   });
 
   return (
     <>
       <form onSubmit={onSubmitForm} encType="multipart/form-data">
         <TextField
+          sx={{ padding: 2 }}
           rows={4}
           multiline
           value={text}
           onChange={onChangeText}
-        ></TextField>
-        <label>
-          <Input
-            accept="image/*"
-            id="contained-button-file"
-            multiple
-            type="file"
-            name="image"
-            onChange={onChangeImages}
-          />
-          <Button variant="contained" component="span">
-            사진
-          </Button>
-        </label>
-        <Button type="submit">확인</Button>
-        {imagePaths.map((v, i) => (
-          <div key={v}>
-            <img src={`http://localhost:3065/${v}`} alt={v} />
-            <div>
-              <Button onClick={onRemoveImage(i)}>제거</Button>
-            </div>
-          </div>
-        ))}
+          fullWidth
+          inputProps={{
+            maxLength: 1500,
+          }}
+          placeholder="내용을 입력해주세요"
+        />
+        <ButtonWrap>
+          <label>
+            <Input
+              accept="image/*"
+              id="contained-button-file"
+              multiple
+              type="file"
+              name="image"
+              onChange={onChangeImages}
+            />
+
+            <Button variant="outlined" component="span" sx={{ mr: 1 }}>
+              사진
+            </Button>
+          </label>
+          {imageLengthError ? (
+            <>
+              <Button disabled>게시글 업로드</Button>
+              <div>10장 이하의 사진만 업로드 가능합니다.</div>
+            </>
+          ) : (
+            <Button type="submit" variant="contained">
+              게시글 업로드
+            </Button>
+          )}
+        </ButtonWrap>
+        <Grid container rowSpacing={1} columns={16} sx={{ padding: 2 }}>
+          {imagePaths.map((v, i) => (
+            <Grid xs={8}>
+              <div key={v}>
+                <ImagePreview src={`http://localhost:3065/${v}`} alt={v} />
+                <DeleteButtonWrap>
+                  <Button onClick={onRemoveImage(i)} sx={{ padding: 0 }}>
+                    <ClearIcon />
+                  </Button>
+                </DeleteButtonWrap>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
       </form>
       <MainMenu />
     </>
